21108eb852947590049f4006df9d37d030ff3937
Keuvain <k94ll13nn3@outlook.fr>
2018-03-18 09:35:34 +0100

    Add test project and update main csproj

---
+++ test/NameInProgress.Tests/Test1.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace NameInProgress.Tests
+{
+    public class Test1
+    {
+        [Fact]
+        public void Test()
+        {
+            'a'.Should().Be('a');
+        }
+    }
+}
