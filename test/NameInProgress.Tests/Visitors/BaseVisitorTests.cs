3bc8e5457b9d20e2a5e98f8619e6cd869cdeafb8
Keuvain <k94ll13nn3@outlook.fr>
2018-04-08 11:03:00 +0200

    Add tests for BaseVisitor

---
+++ test/NameInProgress.Tests/Visitors/BaseVisitorTests.cs
@@ -0,0 +1,32 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using NameInProgress.Visitors;
+using Xunit;
+
+namespace NameInProgress.Tests.Visitors
+{
+    public class BaseVisitorTests
+    {
+        [Fact]
+        public void BaseVisitor_Execute_CallsVisit()
+        {
+            var visitor = A.Fake<BaseVisitor<string>>();
+
+            visitor.Execute(typeof(BaseVisitorTests));
+
+            A.CallTo(() => visitor.Visit(A<ISymbol>._)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void BaseVisitor_Execute_ReturnsGetResults()
+        {
+            var testData = new[] { "test1", "test2", "test3", "test4" };
+
+            var visitor = A.Fake<BaseVisitor<string>>();
+            A.CallTo(() => visitor.GetResults()).Returns(testData);
+
+            visitor.Execute(typeof(BaseVisitorTests)).Should().BeEquivalentTo(testData);
+        }
+    }
+}
