b6e139702c8c1dca709e7fa0cf1b6070ecc18a96
Keuvain <k94ll13nn3@outlook.fr>
2018-04-10 21:29:43 +0200

    Start documentation.

--- src/NameInProgress/Builders/Interfaces/IClassVisitorBuilder.cs
+++ src/NameInProgress/Builders/Interfaces/IClassVisitorBuilder.cs
@@ -4,14 +4,33 @@ using NameInProgress.Visitors;
 
 namespace NameInProgress.Builders
 {
+    /// <summary>
+    /// Interface for exposing the class visitor builder.
+    /// </summary>
     public interface IClassVisitorBuilder
     {
+        /// <summary>
+        /// Builds the visitor and returns it.
+        /// </summary>
+        /// <returns>The visitor.</returns>
         IVisitor<ClassEntity> Build();
 
+        /// <summary>
+        /// Returns a builder for building a condition on generic parameter.
+        /// </summary>
+        /// <returns>The builder.</returns>
         IGenericParameterCondition<IClassVisitorBuilder> WithGenericParameter();
 
+        /// <summary>
+        /// Returns a builder for building a condition on accessibility.
+        /// </summary>
+        /// <returns>The builder.</returns>
         IAccessibilityCondition<IClassVisitorBuilder> WithAccessibility();
 
+        /// <summary>
+        /// Returns a builder for building a condition on name.
+        /// </summary>
+        /// <returns>The builder.</returns>
         INameCondition<IClassVisitorBuilder> WithName();
     }
 }
