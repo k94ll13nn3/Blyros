5286a8fea0b09ec998020eb02344eda29b5c9b5a
Keuvain <k94ll13nn3@outlook.fr>
2018-06-26 12:32:31 +0200

    Add tests project

---
+++ tests/Blyros.Tests/SymbolExtensionsTests.cs
@@ -0,0 +1,55 @@
+﻿using Blyros.Extensions;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using Xunit;
+
+namespace Blyros.Tests
+{
+    public class SymbolExtensionsTests
+    {
+        [Fact]
+        public void IsSpecial_SpecialName_ReturnsTrue()
+        {
+            ISymbol symbol = A.Fake<ISymbol>();
+            A.CallTo(() => symbol.ToString()).Returns("<Module>");
+
+            bool result = symbol.IsSpecial();
+
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsSpecial_NormalName_ReturnsFalse()
+        {
+            ISymbol symbol = A.Fake<ISymbol>();
+            A.CallTo(() => symbol.ToString()).Returns("Module");
+
+            bool result = symbol.IsSpecial();
+
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsSpecial_NormalNameWithStartingAngleBracket_ReturnsFalse()
+        {
+            ISymbol symbol = A.Fake<ISymbol>();
+            A.CallTo(() => symbol.ToString()).Returns("<Module");
+
+            bool result = symbol.IsSpecial();
+
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsSpecial_NormalNameWithEndingAngleBracket_ReturnsFalse()
+        {
+            ISymbol symbol = A.Fake<ISymbol>();
+            A.CallTo(() => symbol.ToString()).Returns("Module>");
+
+            bool result = symbol.IsSpecial();
+
+            result.Should().BeFalse();
+        }
+    }
+}
